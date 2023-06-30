c5442f5aed1ea10c919478a38810fb87a090695d
Andrey Nikitin <anikitin@codio.com>
2023-06-30 12:21:59 +0100

    try filter fragments in pair

--- lib/src/test/pair.test.ts
+++ lib/src/test/pair.test.ts
@@ -105,7 +105,7 @@ test("paired occurrence merging & squashing", t => {
     createOccurrence(right, 1),
   ]);
 
-  const pair = new Pair(left, right);
+  const pair = new Pair(left, right, Number.POSITIVE_INFINITY);
 
   t.is(pair.leftCovered, 12);
   t.is(pair.rightCovered, 10);
